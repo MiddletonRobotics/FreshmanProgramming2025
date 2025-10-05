21e1fb326f81bdab46f5a4256689db362f162b7d
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-10-04 21:05:15 -0400

    intake code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IntakeConstants.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class IntakeConstants {
+    public static final String intakeMotorID = "iM";
+    public static final String intakeBeamBreakID = "bb1";
+    public static final String intakeColorSensorID = "iCS";
+
+    public static double P = 0.01;
+    public static double I = 0.00;
+    public static double D = 0.00;
+    public static double F = 0.00;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,114 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.bylazar.configurables.annotations.IgnoreConfigurable;
+import com.bylazar.telemetry.TelemetryManager;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.controller.PIDFController;
+import com.seattlesolvers.solverslib.controller.wpilibcontroller.SimpleMotorFeedforward;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+
+import org.firstinspires.ftc.teamcode.constants.IntakeConstants;
+
+public class Intake extends SubsystemBase {
+    private Motor intakeMotor;
+    private Motor.Encoder intakeEncoder;
+    private DigitalChannel beamBreak;
+    private RevColorSensorV3 colorSensor;
+
+    @IgnoreConfigurable
+    static TelemetryManager telemetryManager;
+
+    private PIDFController pidfController;
+    private SimpleMotorFeedforward feedforward;
+
+    private double intakeGearRatio;
+
+    public enum BallType {
+        PURPLE(123,102,194),
+        GREEN(0,0,0);
+
+        final int r;
+        final int g;
+        final int b;
+
+        BallType(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public int getR() {
+            return r;
+        }
+
+        public int getG() {
+            return g;
+        }
+
+        public int getB() {
+            return b;
+        }
+    }
+
+    private static Intake instance;
+    public static synchronized Intake getInstance(HardwareMap hMap, TelemetryManager telemetryManager) {
+        if(instance == null) {
+            instance = new Intake(hMap, telemetryManager);
+        }
+
+        return instance;
+    }
+
+    private Intake(HardwareMap hMap, TelemetryManager telemetryManager) {
+        intakeMotor = new Motor(hMap, IntakeConstants.intakeMotorID, Motor.GoBILDA.RPM_1150);
+        intakeEncoder = intakeMotor.encoder;
+        beamBreak = hMap.get(DigitalChannel.class, IntakeConstants.intakeBeamBreakID);
+        colorSensor = hMap.get(RevColorSensorV3.class, IntakeConstants.intakeColorSensorID);
+
+        intakeGearRatio = (48 * 60);
+
+        pidfController = new PIDFController(IntakeConstants.P, IntakeConstants.I, IntakeConstants.D, IntakeConstants.F);
+        feedforward = new SimpleMotorFeedforward(0.0, 0.0, 0.0);
+
+        intakeMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        intakeMotor.setInverted(true);
+
+        this.telemetryManager = telemetryManager;
+    }
+
+    public double getVelocity() {
+        return (intakeEncoder.getCorrectedVelocity() / intakeMotor.getCPR()) * (1 / intakeGearRatio);
+    }
+
+    public boolean isBallPurple() {
+        int colorR = colorSensor.red();
+        int colorG = colorSensor.green();
+        int colorB = colorSensor.blue();
+
+        return (Math.abs(colorR - BallType.PURPLE.getR()) <= 10) || (Math.abs(colorG - BallType.PURPLE.getG()) <= 10) || (Math.abs(colorB - BallType.PURPLE.getB()) <= 10);
+    }
+
+    public boolean isLoaded() {
+        return !beamBreak.getState();
+    }
+
+
+    public void setFlywheelRPM(double desiredRPM) {
+        double output = pidfController.calculate(getVelocity(), desiredRPM);
+        double ff = feedforward.calculate(getVelocity());
+
+        intakeMotor.set(output + ff);
+    }
+
+    @Override
+    public void periodic() {
+        telemetryManager.addData("Intake Velocity", getVelocity());
+        telemetryManager.addData("Intake RPM Setpoint", pidfController.getSetPoint());
+        telemetryManager.addData("Intake BeamBreak Status", isLoaded());
+    }
+}
